2d38e4f66d2fa1dfb49b86e47314ad3aaa9a3a39
= <amanahamed04@gmail.com>
2024-04-29 21:53:00 +0530

    JWT Token Auth

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const User = require("../models/User");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
+var jwt = require("jsonwebtoken");
+
+const JWT_SECRET = "jsutanormalstring$yeaa";
 
 // Create a Using: POST "/api/auth", No Login Required
 router.post("/createuser", async (req, res) => {
@@ -9,9 +13,23 @@ router.post("/createuser", async (req, res) => {
     if (user) {
       return res.status(400).json({ error: "User Already Exists" });
     }
-    const newUser = User(req.body);
-    newUser.save();
-    res.send(newUser);
+    const salt = await bcrypt.genSalt(10);
+
+    const secPass = await bcrypt.hash(req.body.password, salt);
+    const newUser = await User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: secPass,
+    });
+
+    const data = {
+      user: {
+        id: newUser._id,
+      },
+    };
+    const authToken = jwt.sign(data, JWT_SECRET);
+
+    res.json({ authToken });
   } catch (error) {
     console.log(error.message);
     res.status(500).send("Error occured");
